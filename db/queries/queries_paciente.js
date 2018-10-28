2b665d2516b0f0df6ac56fa60918c73c69125be9
sebviro09 <sebviro09@hotmail.com>
2018-10-27 23:57:40 -0600

    add some admin and patient crud queries

---
+++ db/queries/queries_paciente.js
@@ -0,0 +1,46 @@
+/* Solicitar cita */
+db.Appointment.insert(
+	{
+		"area": ,
+		"app_date": ,
+		"observation": ,
+		"id_patient":
+	}
+)
+
+/* Cancelar cita */
+db.Appointment.update(
+	{"app_number": }, 
+	{ $set: 
+		{
+			"status":"Cancelada"
+		} 
+	}
+)
+
+/* Citas asociadas al paciente */
+
+// Por fecha
+db.Appointment.find(
+	{
+    	"app_date": 
+    		{
+        		$gte: ISODate("2011-04-29T00:00:00.000Z"),
+        		$lt: ISODate("2011-06-01T00:00:00.000Z")
+			}
+	}
+)
+
+// Por estado 
+db.Appointment.find(
+	{
+		"status":
+	}
+)
+
+// Por especialidad
+db.Appointment.find(
+	{
+		"area":
+	}
+)
